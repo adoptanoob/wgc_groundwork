1a6cb36e5e917cf32476c6bd23a2f5115d3d7fff
adoptanoob <adoptanoobinrails@gmail.com>
2014-08-22 18:40:52 +0200

    testing workspace

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 require 'sinatra'
 require './lib/profile'
 
-set :application_id, 'EDIT_ME'
-set :secret, 'EDIT_ME'
+set :application_id, 'a95d89fbe3f61b66b68a27777893976daef65e664ed1ab146df11a9db946e5e9'
+set :secret, '5bc07666d0130017733340e2f59f567ad653291065c77fa0a9850a9b4ad9c538'
 set :redirect_uri, 'http://localhost:4567/callback'
 set :site_url, 'http://wegotcoders.com'
 set :session_secret, 'secret'
